dbc974e2eddf6ff16b39d505c73160506b92bf56
Raul Burrieza Garcia <burriezaraul@gmail.com>
2023-11-07 19:30:29 +0100

    add a custom message in game Screen

--- TareaiOS1/GameViewController.swift
+++ TareaiOS1/GameViewController.swift
@@ -13,6 +13,8 @@ class GameViewController: UIViewController {
     @IBOutlet weak var gameImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        adviceLabel.text = UserDefaults.standard.value(forKey: "userName") as! String + " " + adviceLabel.text!
+        gameImageView.image = UIImage(systemName: "gamecontroller.fill")
 
     }
     
